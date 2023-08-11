8576f9e1603a0c07c9699c2430f47f1f4b1ab489
Swifter <swifter1243@gmail.com>
2023-08-11 15:27:57 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Enlighten
+{
+    [Plugin("Enlighten")]
+    public class Enlighten
+    {
+        [Init]
+        private void Init()
+		{
+            Debug.Log("Hi");
+		}
+    }
+}
