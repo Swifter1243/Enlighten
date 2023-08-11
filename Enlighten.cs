2e4a2dac0cda2217dc6c1f9f0c579d45c4b59909
Swifter <swifter1243@gmail.com>
2023-08-11 16:09:46 -0400

    basic registration

---
+++ Enlighten.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace Enlighten
+{
+    [Plugin("Enlighten")]
+    public class Enlighten
+    {
+        private EventGridContainer events;
+        private UI ui;
+
+        [Init]
+        private void Init()
+		{
+            SceneManager.sceneLoaded += SceneLoaded;
+            ui = new UI(this);
+		}
+
+        private void SceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            if (arg0.buildIndex == 3) // Mapping Scene
+			{
+                events = UnityEngine.Object.FindObjectOfType<EventGridContainer>();
+			}
+        }
+    }
+}
---
+++ UI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enlighten
+{
+	class UI
+	{
+		private Enlighten plugin;
+
+		public UI(Enlighten plugin)
+		{
+			this.plugin = plugin;
+
+			var button = new ExtensionButton();
+			button.Tooltip = "Enlighten";
+			ExtensionButtons.AddButton(button);
+		}
+	}
+}
