0eac75a34cf3ac14f480be3d1a201090d6042898
Swifter <swifter1243@gmail.com>
2025-03-12 00:18:40 -0400

    StripGeneratorMode.cs stub

--- src/Enlighten.Plugin/src/Core/Modes/Strip Generator/StripGeneratorMode.cs
+++ src/Enlighten.Plugin/src/Core/Modes/Strip Generator/StripGeneratorMode.cs
@@ -1,7 +1,12 @@
-﻿namespace Enlighten.Core.Strip_Generator
+﻿using System.Collections.Generic;
+using Beatmap.Base;
+namespace Enlighten.Core.Strip_Generator
 {
 	public class StripGeneratorMode : EnlightenMode
 	{
-
+		public override IEnumerable<BeatmapAction> Execute(BaseEvent[] events, ActionTracker actionTracker)
+		{
+			return actionTracker.Finish();
+		}
 	}
 }
