558a6c837eba0ce4b7e769889fd339a7a0120fc9
Swifter <61858676+Swifter1243@users.noreply.github.com>
2025-03-12 19:11:16 -0400

    float value editor hookup maybe

---
+++ src/Enlighten.Plugin/src/UI/Modes/Main Effects/MainEffectsUI.cs
@@ -0,0 +1,22 @@
+﻿using Enlighten.Core;
+using UnityEngine;
+namespace Enlighten.UI
+{
+	public class MainEffectsUI : MonoBehaviour
+	{
+		private MainEffectsMode m_mode;
+		private FloatValueEditorUI m_floatValueEditor;
+
+		public void Initialize(MainEffectsMode mode)
+		{
+			m_mode = mode;
+			
+			GameObject content = transform.Find("Content").gameObject;
+			GameObject leftContent = content.transform.Find("LeftContent").gameObject;
+			GameObject rightContent = content.transform.Find("RightContent").gameObject;
+			FloatValueEditorUI floatValueEditorUI = rightContent.transform.Find("ValueEditorWindow").gameObject.AddComponent<FloatValueEditorUI>();
+			
+			
+		}
+	}
+}
