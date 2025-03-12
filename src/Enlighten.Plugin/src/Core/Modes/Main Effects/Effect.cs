fb26104752a5b5d75c9d527677b517355e23247b
Swifter <swifter1243@gmail.com>
2025-03-11 22:14:19 -0400

    add parameters to effects

--- src/Enlighten.Plugin/src/Core/Modes/Main Effects/Effect.cs
+++ src/Enlighten.Plugin/src/Core/Modes/Main Effects/Effect.cs
@@ -1,10 +1,16 @@
-﻿using Beatmap.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Beatmap.Base;
+using JetBrains.Annotations;
 namespace Enlighten.Core
 {
 	public abstract class Effect
 	{
 		public bool m_isEnabled = false;
+		private List<BaseParameter> m_parameters;
+		public List<BaseParameter> Parameters => m_parameters ?? (m_parameters = GetParameters().ToList());
 
+		protected abstract IEnumerable<BaseParameter> GetParameters();
 		public abstract BaseEvent[] ProcessEvents(BaseEvent[] events, ActionTracker actionTracker);
 	}
 }
