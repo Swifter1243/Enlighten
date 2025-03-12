b1984f8030691cca1969e993b53519ab5c081378
Swifter <swifter1243@gmail.com>
2025-03-11 22:55:52 -0400

    saturation effect

--- src/Enlighten.Plugin/src/Core/Modes/Main Effects/Effects/SaturationEffect.cs
+++ src/Enlighten.Plugin/src/Core/Modes/Main Effects/Effects/SaturationEffect.cs
@@ -1,7 +1,23 @@
-﻿namespace Enlighten.Core
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace Enlighten.Core
 {
-	public class SaturationEffect : Effect
+	public class SaturationEffect : SimpleColorEffect
 	{
+		private readonly FloatParameter m_offset = new FloatParameter(0);
 
+		protected override IEnumerable<BaseParameter> GetParameters()
+		{
+			yield return m_offset;
+		}
+
+		protected override Color GetColor(float normalTime, Color color)
+		{
+			float offset = m_offset.Interpolate(normalTime);
+
+			Color.RGBToHSV(color, out float h, out float s, out float v);
+			s = Mathf.Clamp01(s + offset);
+			return Color.HSVToRGB(h, s, v);
+		}
 	}
 }
