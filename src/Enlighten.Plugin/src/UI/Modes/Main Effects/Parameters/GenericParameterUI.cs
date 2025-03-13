a367cba3023f769417a2ecfe950edaef7f8f328d
Swifter <swifter1243@gmail.com>
2025-03-13 01:56:02 -0400

    dont use keyframe instances, they don't work!

--- src/Enlighten.Plugin/src/UI/Modes/Main Effects/Parameters/GenericParameterUI.cs
+++ src/Enlighten.Plugin/src/UI/Modes/Main Effects/Parameters/GenericParameterUI.cs
@@ -7,32 +7,51 @@ namespace Enlighten.UI
 	public abstract class BaseParameterUI : MonoBehaviour {}
 	public abstract class GenericParameterUI<T, TP> : BaseParameterUI where TP : GenericParameter<T>
 	{
-		private GenericParameter<T>.Keyframe m_selectedKeyframe;
-		protected T CurrentValue => m_selectedKeyframe.m_value;
+		private TP m_parameter;
+		private int m_selectedKeyframeIndex;
+
+		private GenericParameter<T>.Keyframe SelectedKeyframe
+		{
+			get => m_parameter.m_keyframes[m_selectedKeyframeIndex];
+			set => m_parameter.m_keyframes[m_selectedKeyframeIndex] = value;
+		}
+
+		protected T CurrentValue
+		{
+			get => SelectedKeyframe.m_value;
+			private set
+			{
+				GenericParameter<T>.Keyframe k = SelectedKeyframe;
+				k.m_value = value;
+				SelectedKeyframe = k;
+			}
+		}
 		public event Action<GenericParameterUI<T, TP>> onUIValueChanged;
 
 		protected abstract void InitializeInternal(TP parameter);
 
 		public void Initialize(TP parameter)
 		{
+			m_parameter = parameter;
 			Text nameText = transform.Find("Name").GetComponent<Text>();
-			nameText.text = parameter.m_name;
-			ChroMapperUtils.AddTooltip(nameText.gameObject, parameter.m_description);
-			m_selectedKeyframe = parameter.m_keyframes[0];
+			nameText.text = m_parameter.m_name;
+			ChroMapperUtils.AddTooltip(nameText.gameObject, m_parameter.m_description);
 			InitializeInternal(parameter);
 			UpdateUI();
 		}
 
 		public void SetKeyframeInfo(T value, float time)
 		{
-			m_selectedKeyframe.m_time = time;
-			m_selectedKeyframe.m_value = value;
+			GenericParameter<T>.Keyframe k = SelectedKeyframe;
+			k.m_time = time;
+			k.m_value = value;
+			SelectedKeyframe = k;
 			UpdateUI();
 		}
 
-		public void SetActiveKeyframe(GenericParameter<T>.Keyframe keyframe)
+		public void SetActiveKeyframeIndex(int index)
 		{
-			m_selectedKeyframe = keyframe;
+			m_selectedKeyframeIndex = index;
 			UpdateUI();
 		}
 
@@ -40,7 +59,7 @@ namespace Enlighten.UI
 
 		protected void SetCurrentValue(T value)
 		{
-			m_selectedKeyframe.m_value = value;
+			CurrentValue = value;
 			onUIValueChanged?.Invoke(this);
 		}
 	}
