75d27edc77d175d141c0817662457eaf9043c805
Swifter <swifter1243@gmail.com>
2025-03-12 21:36:44 -0400

    OptionPanelUI.cs partial initialization

--- src/Enlighten.Plugin/src/UI/Modes/Main Effects/OptionPanelUI.cs
+++ src/Enlighten.Plugin/src/UI/Modes/Main Effects/OptionPanelUI.cs
@@ -7,24 +7,31 @@ namespace Enlighten.UI
     {
         private BundleLoading.Assets m_assets;
         private Effect m_effect;
-        
+
         public void Initialize(BundleLoading.Assets assets, Effect effect)
         {
             m_assets = assets;
             m_effect = effect;
-            
-            GameObject go = new GameObject("OptionPanel");
-            
-            foreach (BaseParameter effectParameter in effect.Parameters)
+
+            Transform parametersParent = transform.Find("Parameters");
+
+            foreach (BaseParameter effectParameter in m_effect.Parameters)
             {
                 switch (effectParameter)
                 {
+                case RangeParameter rangeParameter:
+                    Instantiate(m_assets.m_rangeParameter, parametersParent);
+                    break;
                 case FloatParameter floatParameter:
-                        
+                    Instantiate(m_assets.m_floatParameter, parametersParent);
+                    break;
+                case BoolParameter boolParameter:
+                    Instantiate(m_assets.m_boolParameter, parametersParent);
+                    break;
                 }
             }
         }
-        
-        
+
+
     }
 }
