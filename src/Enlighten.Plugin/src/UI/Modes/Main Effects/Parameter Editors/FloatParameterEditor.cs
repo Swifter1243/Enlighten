e483d638414a6770503e1368c44b452e6b572628
Swifter <swifter1243@gmail.com>
2025-03-13 03:03:42 -0400

    fixed(?) parameter editors

--- src/Enlighten.Plugin/src/UI/Modes/Main Effects/Parameter Editors/FloatParameterEditor.cs
+++ src/Enlighten.Plugin/src/UI/Modes/Main Effects/Parameter Editors/FloatParameterEditor.cs
@@ -1,8 +1,14 @@
 ﻿using Enlighten.Core;
+using UnityEngine;
 namespace Enlighten.UI
 {
 	internal class FloatParameterEditor : ChartParameterEditor<float>
 	{
+		protected override float ChartPositionYToValue(float y) => y;
 
+		protected override Vector2 HandleKeyframeMove(Vector2 chartPosition)
+		{
+			return chartPosition;
+		}
 	}
 }
